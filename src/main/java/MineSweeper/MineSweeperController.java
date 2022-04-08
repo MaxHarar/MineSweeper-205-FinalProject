b21c53e80a9a47849fdeb676adfb3a4725f12350
Sam Vickers <stv002@bucknell.edu>
2022-04-08 02:15:51 -0400

    JavaFX UI prototype

---
+++ src/main/java/MineSweeper/MineSweeperController.java
@@ -0,0 +1,43 @@
+/* *****************************************
+ * CSCI205 -Software Engineering and Design
+ * Spring2022* Instructor: Prof. Brian King
+ * Name: Sam Vickers* Section: 1
+ * Date: 4/8/2022
+ * Time: 1:10 AM
+ * Project: csci205_FinalProject
+ * Package: MineSweeper
+ * Class: MineSweeperController
+ * Description:
+ ******************************************/
+package MineSweeper;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+public class MineSweeperController {
+
+    private MineSweeperView theView;
+    private Game game;
+
+    public MineSweeperController(MineSweeperView view, Game game){
+        this.theView = view;
+        this.game = game;
+        initBindings();
+    }
+
+    private void initBindings(){
+        int rows = game.getRowCount();
+        int cols = game.getColCount();
+
+        Label[][] labels = theView.getLabels();
+        Cell[][] cells = game.getCells();
+
+        for (int r = 0; r < rows; r++){
+            for (int c = 0; c < cols; c++){
+                labels[r][c].textProperty().bind(new SimpleStringProperty("" + cells[r][c].getDisplayChar()));
+            }
+        }
+    }
+}
---
+++ src/main/java/MineSweeper/MineSweeperMain.java
@@ -0,0 +1,47 @@
+/* *****************************************
+ * CSCI205 -Software Engineering and Design
+ * Spring2022* Instructor: Prof. Brian King
+ * Name: Sam Vickers* Section: 1
+ * Date: 4/8/2022
+ * Time: 1:09 AM
+ * Project: csci205_FinalProject
+ * Package: MineSweeper
+ * Class: Main
+ * Description:
+ ******************************************/
+package MineSweeper;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MineSweeperMain extends Application {
+
+    private MineSweeperView theView;
+    private MineSweeperController theController;
+    private Game game;
+
+    @Override
+    public void init() throws Exception{
+        super.init();
+        game = new Game();
+        this.theView = new MineSweeperView(this.game);
+        this.theController = new MineSweeperController(this.theView, this.game);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Scene scene = new Scene(theView.getRoot());
+
+        this.theController = new MineSweeperController(this.theView, this.game);
+
+        primaryStage.setTitle("MineSweeper");
+        primaryStage.setScene(scene);
+        primaryStage.sizeToScene();
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
